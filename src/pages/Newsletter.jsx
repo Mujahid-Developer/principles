54a2480995c90f73c72d8c5456855d8a5dcc12fa
Mujahid-Developer <mujahidulislam575@gmail.com>
2022-04-26 23:17:43 +0600

    add route to newsletter

---
+++ src/pages/Newsletter.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "../components/Footer";
+import NavBar from "../components/NavBar";
+
+const Newsletter = () => {
+  return (
+    <div className="flex flex-col justify-between h-screen">
+      <NavBar />
+      <h1>Mailchimp Integrat here</h1>
+      <Footer />
+    </div>
+  );
+};
+
+export default Newsletter;
